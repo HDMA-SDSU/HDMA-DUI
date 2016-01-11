028e44fd1de87676d2399427c0e493d0ae27ca5e
GeogAvgJ <jiuean.yang@gmail.com>
2016-01-11 08:57:23 -0800

    Update Menu
    (1) add morris chart
    (2) add excel to db
    (3) add js-xlsx to read and process excel data
    (4) change width of two panels

---
+++ js/chart.js
@@ -0,0 +1,68 @@
+var countyNames = ['Alameda','Amador','Butte','Calaveras','Colusa','Contra Costa','Del Norte','El Dorado','Fresno','Humboldt','Imperial','Inyo','Kern','Kings','Lake','Lassen','Los Angeles','Madera','Marin','Mariposa','Mendocino','Merced','Modoc','Mono','Monterey','Napa','Nevada','Orange','Placer','Plumas','Riverside','Sacramento','San Benito','San Bernardino','San Diego','San Francisco','San Joaquin','San Luis Obispo','San Mateo','Santa Barbara','Santa Clara','Santa Cruz','Sierra','Santa Cruz','Siskiyou','Solano','Sonoma','Stanislaus','Sutter','Tehama','Trinity','Tulare','Tuolumne','Ventura','Yolo']
+
+var typeNames = ['First Offender','18 Month','Wet Reckless']
+
+var selectedCounty = ''
+var selectedType = ''
+
+
+
+for (var i in countyNames) {
+  $('#countyDropdown').append('<li><a href="#">' + countyNames[i] +  '</a></li>')
+}
+
+for (var i in typeNames) {
+  $('#typeDropdown').append('<li><a href="#">' + typeNames[i] +  '</a></li>')
+}
+
+
+$('#countyDropdown li').on('click', function(){
+    selectedCounty = $(this).text();
+    $('#countyBox').html($(this).text() + ' <span class="caret"></span>');
+    $('#countyBox').val($(this).text());
+});
+
+
+$('#typeDropdown li').on('click', function(){
+    selectedType = $(this).text();
+    $('#typeBox').html($(this).text() + ' <span class="caret"></span>');
+    $('#typeBox').val($(this).text());
+});
+
+
+$('#getCountyDataBtn').on('click', function(){
+    var countyIndex = countyNames.indexOf(selectedCounty);
+    var typeIndex = typeNames.indexOf(selectedType);
+    
+    console.log(countyIndex);
+    console.log(typeIndex);
+    
+    getExcelValue(countyIndex, typeIndex, function (arr){
+        console.log(arr);
+        
+        addRateChart(arr);
+        
+    });
+});
+
+
+
+
+function addRateChart(inputArr) {    
+    
+    $('#rateChart').empty();
+
+    new Morris.Line({
+        element: 'rateChart',
+        data:inputArr,
+        smooth:false,
+        xkey: 'y',
+        xLabels:'year',
+        ykeys: ['C','T','X'],
+        labels: ['C_Rate','T_Rate','X_Rate'],
+        lineColors : ['#3371FF','#FF5733','#009933'],
+        xLabelAngle: 70,
+        yLabelFormat: function(y){return (y *100).toFixed(2) + ' %'; },
+        hideHover: 'auto'
+    });
+}
---
+++ js/read_excel.js
@@ -0,0 +1,142 @@
+var dataSheetNames = ['FO_C','FO_T','FO_X','18_Months_C','18_Months_T','18_Months_X','Wet_reckless_C','Wet_reckless_T','Wet_reckless_X']
+var yearStart = 1995
+var yearEnd = 2014
+
+function getExcelValue(countyIndex, typeIndex, callback) {
+    
+    var resultsArr = [];
+    var C_Rate_Arr = [];
+    var T_Rate_Arr = [];
+    var X_Rate_Arr = [];
+    
+    var url = "db/dui_rate_data.xlsx";
+    var oReq = new XMLHttpRequest();
+    
+    oReq.open("GET", url, true);
+    oReq.responseType = "arraybuffer";
+
+    oReq.onload = function (e) {
+        var arraybuffer = oReq.response;
+
+        /* convert data to binary string */
+        var data = new Uint8Array(arraybuffer);
+        var arr = new Array();
+        for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+        var bstr = arr.join("");
+
+        /* Call XLSX */
+        var workbook = XLSX.read(bstr, {
+            type: "binary"
+        });
+        
+        var dataJson = to_json(workbook);
+        
+        var sheetCounterStart = parseInt(typeIndex) * 3
+        var sheetCounterEnd = sheetCounterStart + 2
+        
+        for (i = sheetCounterStart; i< sheetCounterEnd + 1; i++ ){
+            var sheetName = dataSheetNames[i];
+            
+            if (sheetName.slice(-1) == 'C'){
+                var countyData = dataJson[sheetName][countyIndex]
+                
+                for (j = yearStart; j < yearEnd+1; j++){
+                    
+                    if (countyData[j] === 'null'){
+                        C_Rate_Arr.push(null);
+                    }
+                    else{
+                        C_Rate_Arr.push(parseFloat(countyData[j]))
+                    }
+                    
+                }               
+                console.log(C_Rate_Arr)
+            }
+            else if (sheetName.slice(-1) == 'T'){
+                var countyData = dataJson[sheetName][countyIndex]
+                
+                for (j = yearStart; j < yearEnd+1; j++){
+                    
+                    if (countyData[j] === 'null'){
+                        T_Rate_Arr.push(null);
+                    }
+                    else{
+                        T_Rate_Arr.push(parseFloat(countyData[j]))
+                    }
+                }
+            }
+            else if (sheetName.slice(-1) == 'X'){
+                var countyData = dataJson[sheetName][countyIndex]
+                
+                for (j = yearStart; j < yearEnd+1; j++){
+                    
+                    if (countyData[j] === 'null'){
+                        X_Rate_Arr.push(null);
+                    }
+                    else{
+                        X_Rate_Arr.push(parseFloat(countyData[j]))
+                    }
+                }
+            }
+        }
+        
+        for ( i = (yearStart - 1995); i < (yearEnd - 1995 + 1); i++){
+            var year = (i + 1995).toString();
+            var C_rate = C_Rate_Arr[i];
+            var T_rate = T_Rate_Arr[i];
+            var X_rate = X_Rate_Arr[i];
+            
+            var inputJson = {};
+            inputJson.y = year;
+            inputJson.C = C_rate;
+            inputJson.T = T_rate;
+            inputJson.X = X_rate;
+                
+            resultsArr.push(inputJson)
+        }
+        
+//        getCell(workbook, typeIndex, "T15" ,function(result){
+//            resultsArr.push(result);
+//        });
+        
+        callback(resultsArr);
+        /* DO SOMETHING WITH workbook HERE */
+    }
+    oReq.send();
+}
+
+/* set up XMLHttpRequest */
+
+
+var X = XLSX;
+
+function to_json(workbook) {
+    var result = {};
+    workbook.SheetNames.forEach(function (sheetName) {
+        var roa = X.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+        if (roa.length > 0) {
+            result[sheetName] = roa;
+        }
+    });
+    return result;
+}
+
+
+function getCell(workbook, typeIndex, address_of_cell, callback) {
+    
+    console.log("getCell Called!")
+    
+    var address_of_cell = desired_cell;
+    var sheetName = workbook.SheetNames[typeIndex];
+
+    /* Get worksheet */
+    var worksheet = workbook.Sheets[worksheet];
+
+    /* Find desired cell */
+    var desired_cell = worksheet[address_of_cell];
+
+    /* Get the value */
+    var desired_value = desired_cell.v;
+
+    callback(desired_value);
+}
